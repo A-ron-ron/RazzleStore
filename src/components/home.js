178806a880e043df14bcbd9a8211cfaf6a9a5908
A-ron-ron <services@arronhogg.co.uk>
2021-01-06 23:37:40 +0000

    context removed on home

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
-import { Context } from '../context';
+// import { Context } from '../context';
 
 export default function Home() {
 
-  const {Hello} = useContext(Context)
+  // const {Hello} = useContext(Context)
 
     return (
       <div>
-        <p> {Hello} Context Trial </p>
+        <p> Context Trial </p>
       </div>
     );
   }
