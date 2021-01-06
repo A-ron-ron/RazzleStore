0f3ae10660186c666dc2bbdadd8ad6ffd35b686b
A-ron-ron <services@arronhogg.co.uk>
2021-01-06 23:06:47 +0000

    revert

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
-import { Context } from '../context';
+import React from 'react';
 
 export default function Home() {
 
-  const {Hello} = useContext(Context)
-
     return (
       <div>
-        <p> {Hello} Context Trial </p>
+        <p> Context Trial </p>
       </div>
     );
   }
